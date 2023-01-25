0206d12ec03beb7908638784f67a72c1dd9b223c
seanlb02 <seangyuris1@gmail.com>
2023-01-25 14:53:41 +1100

    completed user update memo route

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -6,17 +6,37 @@ import uniqueValidator from "mongoose-unique-validator"
 // all middleware functions to handle 'users' resources 
 
 
-// function to get all a specific users data (for profile rendering) 
+    // middleware function to get all a specific users data (for profile rendering) 
 
     const getUserData = async function(req, res, next) {
         // note: the req.params are set by the JWT authenticator middleware! and not in the route URL
         const {username} = req.params
         const userData = await UsersModel.find({username:username}).select('-_id -__v -password')
         res.send(userData)
-
     }
+
+    // middleware function get all user data (admins only)
+    const getAllUsers = async function(req, res, next) {
+        const users = await UsersModel.find().select('-__v -password')
+        res.send(users)
+        next();
+    }
+
+    //  function for user to update their memo 
+    const updateMemo = async function(req, res, next) {
+        const {username} = req.params 
+        const {memo} = req.body
+        
+        const filter = { username: username };
+        const update = { memo: memo };
+        const newMemo = await UsersModel.findOneAndUpdate(filter, update)
+        res.send({'success': 'memo updated successfully'})
+    }
+
 
     export {
         getUserData,
+        getAllUsers,
+        updateMemo
     }
 
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,18 +1,24 @@
 import express from 'express';
 
 // import controllers/middleware 
-import { getUserData } from '../controllers/user_controllers.js';
+import { getAllUsers, getUserData, updateMemo } from '../controllers/user_controllers.js';
 import { authenticateToken, checkAdmin } from '../controllers/auth_controllers.js';
 
 const router = express.Router();
 
 // admin protected route to fetch all users in database 
-router.get('/:adminusername/all', authenticateToken, checkAdmin)
+router.get('/all', authenticateToken, checkAdmin, getAllUsers)
 
 
 // route to get a logged in user's data (identity taken from JWT autenticator) 
 router.get ('/data', authenticateToken, getUserData)
  
+//  route to update a users's memo 
+router.patch('/memo/update', authenticateToken, updateMemo)
+
+// route for logged in user to delete their account
+router.delete('/account/delete', authenticateToken )
+
 
 // export the router 
  export default router 
