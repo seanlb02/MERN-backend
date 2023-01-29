de940e994f8bdd91e2fd21aeba9bd3115101f2d2
seanlb02 <seangyuris1@gmail.com>
2023-01-29 13:28:39 +1100

    re-adding landing page

--- routes/landing_routes.js
+++ routes/landing_routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router()
 
 router.get('/', (req, res) => {
-        res.send(<div>Welcome to the Serene backend API</div>);
+        res.send('Welcome to the Serene backend API');
     })
 
 export default router;
