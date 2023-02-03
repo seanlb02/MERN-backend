3cb6f17866cc5fd4aa20a3da5ac2710f2e93f33c
seanlb02 <seangyuris1@gmail.com>
2023-02-03 15:33:06 +1100

    removing timestamp from query

--- controllers/entries_controllers.js
+++ controllers/entries_controllers.js
@@ -34,7 +34,7 @@ const getUserEntries = async function (req, res, next) {
         const pastDay = currentDateTime.setDate(currentDateTime.getDate() - 1)
         const {username} = req.params.username;
         try {
-            const tag = await EntriesModel.find({username: username, timestamp: {$gte:pastDay}}, 'tags timestamp').select('-_id')
+            const tag = await EntriesModel.find({username: username, timestamp: {$gte:pastDay}}, 'tags -_id').select('-_id')
             res.send(tag);
             next();
         }
